322e6403bb0ccb4011f5b8e34a9b55b425df5f94
Erik Michaels-Ober <sferik@gmail.com>
2011-02-25 16:34:01 -0500

    Zoom after search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+  def index
+    # Default coordinates: Boston center
+    @lat, @lng = 42.358431, -71.059773
+    @zoom = 15
+    address, city, state = params[:address], params[:city], params[:state]
+    if address && city && state
+      @lat, @lng = Address.find_lat_lng("#{address}, #{city}, #{state}")
+      @zoom = 18
+    end
+  end
 end
