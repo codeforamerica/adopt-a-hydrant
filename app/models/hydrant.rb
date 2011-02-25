b47c1989639f37f5dc027d8b539997699df54359
Erik Michaels-Ober <sferik@gmail.com>
2011-02-25 16:51:56 -0500

    Set default number of hydrants to 100

--- app/models/hydrant.rb
+++ app/models/hydrant.rb
@@ -1,7 +1,7 @@
 class Hydrant < ActiveRecord::Base
   belongs_to :user
 
-  def self.find_closest(lat, lng, limit=20)
+  def self.find_closest(lat, lng, limit=100)
     Hydrant.find_by_sql(["SELECT *, (3959 * ACOS(COS(RADIANS(?)) * COS(RADIANS(lat)) * COS(radians(lng) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(lat)))) AS distance FROM hydrants ORDER BY distance LIMIT ?", lat, lng, lat, limit])
   end
 end
