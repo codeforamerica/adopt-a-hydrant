22108573870c763e39935aee9c836c218b073017
Erik Michaels-Ober <sferik@gmail.com>
2011-02-25 16:49:45 -0500

    Display hydrants on the map

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @zoom = 15
     # Default coordinates: Boston center
     @lat, @lng = 42.358431, -71.059773
-    @zoom = 15
+    @hydrants = []
     address, city, state = params[:address], params[:city], params[:state]
     if address && city && state
-      @lat, @lng = Address.find_lat_lng("#{address}, #{city}, #{state}")
       @zoom = 18
+      @lat, @lng = Address.find_lat_lng("#{address}, #{city}, #{state}")
+      @hydrants = Hydrant.find_closest(@lat, @lng)
     end
   end
 end
