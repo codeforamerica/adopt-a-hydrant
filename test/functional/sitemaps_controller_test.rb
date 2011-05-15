67b4698b5e8ef3f5817cd0bce0263a12a89f1974
Erik Michaels-Ober <sferik@gmail.com>
2011-05-15 11:30:32 -0700

    Add tests for sitemaps controller

---
+++ test/functional/sitemaps_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class SitemapsControllerTest < ActionController::TestCase
+  test 'should return an XML sitemap' do
+    get :index, :format => 'xml'
+    assert_response :success
+  end
+end
