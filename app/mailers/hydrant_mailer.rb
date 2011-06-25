fb908ed57475aad00a97edc5f31953af59521d70
Erik Michaels-Ober <sferik@gmail.com>
2011-06-25 02:53:37 -0700

    Fix tests

--- app/mailers/hydrant_mailer.rb
+++ app/mailers/hydrant_mailer.rb
@@ -4,10 +4,10 @@ class HydrantMailer < ActionMailer::Base
   def reminder_email(hydrant)
     @hydrant = hydrant
     @user = hydrant.user
-    mail(
+    mail({
       :to => hydrant.user.email,
       :from => "reminder@#{default_url_options[:host]}",
       :subject => ["Remember to Shovel", hydrant.name].compact.join(' '),
-    )
+    })
   end
 end
