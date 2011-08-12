9992a59a9c0c7dd9cff51d361824e61eccb5ce7e
Erik Michaels-Ober <sferik@gmail.com>
2011-08-11 17:12:18 -0700

    Fix version number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110809190528) do
+ActiveRecord::Schema.define(:version => 4) do
 
   create_table "hydrants", :force => true do |t|
     t.datetime "created_at"
