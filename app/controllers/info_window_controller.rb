34f9b75e79fff5fcb90fdd9ab64eaddf0ed8e057
Erik Michaels-Ober <sferik@gmail.com>
2011-08-29 14:51:44 -0700

    Add flash messages

--- app/controllers/info_window_controller.rb
+++ app/controllers/info_window_controller.rb
@@ -1,6 +1,11 @@
 class InfoWindowController < ApplicationController
   def index
     @hydrant = Hydrant.find_by_id(params[:hydrant_id])
+    if params[:flash]
+      params[:flash].each do |key, message|
+        flash.now[key.to_sym] = message
+      end
+    end
     if @hydrant.adopted?
       if user_signed_in? && current_user.id == @hydrant.user_id
         render("users/thank_you")
