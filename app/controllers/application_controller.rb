8de04a6cab1781506c7ec9e519b52f6614f1151a
Erik Michaels-Ober <sferik@gmail.com>
2011-08-29 17:50:51 -0700

    Set flash in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_flash_from_params
+
+protected
+
+  def set_flash_from_params
+    if params[:flash]
+      params[:flash].each do |key, message|
+        flash.now[key.to_sym] = message
+      end
+    end
+  end
 end
