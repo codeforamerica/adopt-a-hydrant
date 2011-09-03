5a6c5132ae8a41b0c433cfb9bd4ab9f55f26adf8
Erik Michaels-Ober <sferik@gmail.com>
2011-09-03 13:27:23 -0700

    Change production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,14 +8,14 @@ AdoptAHydrant::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Enable Rails's static asset server
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
