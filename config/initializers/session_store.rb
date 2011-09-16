e1d60ebded0bd49343f796fb7f6b9bdedcc14ff8
Erik Michaels-Ober <sferik@gmail.com>
2011-09-15 22:00:04 -0700

    Finish hydrant refactoring

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AdoptAThing::Application.config.session_store :cookie_store, :key => '_adopt-a-hydrant_session'
+AdoptAThing::Application.config.session_store :cookie_store, :key => '_adopt-a-thing_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
