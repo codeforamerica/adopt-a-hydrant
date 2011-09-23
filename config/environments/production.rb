fd797f4fd1a90eea4a3ede6258c4b7cb122b3007
Erik Michaels-Ober <sferik@gmail.com>
2011-09-23 11:19:02 -0700

    Configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,3 +60,12 @@ AdoptAThing::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
