0f37c38657542f110535eb71cf57508fb9c421f2
Erik Michaels-Ober <sferik@gmail.com>
2011-10-10 07:48:38 -0700

    Fix tests

--- test/functional/main_controller_test.rb
+++ test/functional/main_controller_test.rb
@@ -5,7 +5,6 @@ class MainControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_select 'title', 'Adopt a Hydrant'
-    assert_select 'h1', 'Adopt a Hydrant'
     assert_select 'p#tagline', 'Claim responsibility for shoveling out a fire hydrant after it snows.'
     assert_select 'form' do
       assert_select '[action=?]', '/'
