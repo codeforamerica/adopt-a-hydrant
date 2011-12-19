afb2b1ab6b1212c7a54241dee050caad43601110
Erik Michaels-Ober <sferik@gmail.com>
2011-12-19 01:36:27 -0800

    Add precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ AdoptAThing::Application.configure do
   config.assets.compress = true
 
   # Fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
