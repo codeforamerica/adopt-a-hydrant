0cd8307c36a839d393b2c829c3bf8f135383f05c
Erik Michaels-Ober <sferik@gmail.com>
2012-01-02 11:37:57 -0800

    Update validates_formatting_of dependency to version 0.4.0
    Add validation for phone numbers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
     :password, :password_confirmation, :remember_me, :sms_number, :state,
     :voice_number, :zip
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_formatting_of :sms_number, :using => :us_phone, :allow_blank => true
+  validates_formatting_of :voice_number, :using => :us_phone, :allow_blank => true
   validates_formatting_of :zip, :using => :us_zip, :allow_blank => true
   validates_presence_of :name
   has_many :reminders_to, :class_name => "Reminder", :foreign_key => "to_user_id"
