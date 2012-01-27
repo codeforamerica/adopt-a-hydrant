b311063ed79024663efd0ba51e2d0ada81e287c6
Erik Michaels-Ober <sferik@gmail.com>
2012-01-27 13:11:44 -0800

    Add uniqueness constraint on city_id

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,5 +1,6 @@
 class Thing < ActiveRecord::Base
   include Geokit::Geocoders
+  validates_uniqueness_of :city_id, :allow_nil => true
   validates_presence_of :lat, :lng
   belongs_to :user
   has_many :reminders
