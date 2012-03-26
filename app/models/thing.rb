90e5c4f684974f9e3e3583af1295d9da8514a678
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-03-26 12:55:11 -0400

    adding note

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -5,7 +5,7 @@ class Thing < ActiveRecord::Base
   belongs_to :user
   has_many :reminders
 
-  def self.find_closest(lat, lng, limit=10)
+  def self.find_closest(lat, lng, limit=10) #TODO: filter by user_id, filter by city_id
     query = <<-SQL
       SELECT *, (3959 * ACOS(COS(RADIANS(?)) * COS(RADIANS(lat)) * COS(radians(lng) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(lat)))) AS distance
       FROM things
