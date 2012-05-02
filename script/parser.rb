8e1c7c477c74a560e28150fee2b8eb13bd461d60
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-02 13:56:16 -0400

    adding parser

--- script/parser.rb
+++ script/parser.rb
@@ -1,45 +1,40 @@
-require 'libxml'
+# Thanks to the example set at https://github.com/xml4r/libxml-ruby/wiki/Using-the-Reader-API
+# http://snippets.dzone.com/posts/show/5051
+# http://www.informit.com/articles/article.aspx?p=683059&seqNum=18
 
-module Parser
-  include LibXML
-  
-  class KMLParser
-    # def extract(filename) #kml
-    #   container = libxmlmethodthatreadsxmlfile(filename)
-    #   return container
-    # end
-    # 
-    # def pourOutData(container)
-    #   #instantiate string here?
-    #   for each placemark in container
-    #     for each lng, lat in placemark
-    #       lng = lng
-    #       lat = lat
-    #       string.append."Thing.create(:city_id => 1, :lng => ("lng" = lng), :lat => ("lat" = lat))\n"
-    #   return string.truncatetrailing\n
-    #  
-    # def transform(container) #seeds.rb
-    #   file = create("seeds.rb")
-    #   file.append(pourOutData(container))
-    #   file.close
-    # end
-    # 
-    def load
-      system("rake db:seed")
-    end
-    
-    # def populateDatabase(filename)
-    #   transform(extract(filename))
-    #   load
-    # end
-  end
-  
-  program = KMLParser.new()
-  program.populateDatabase()
+require 'rubygems'
+require 'xml'
+
+def extract(filename) #kml
+  #container = libxmlmethodthatreadsxmlfile(filename) #../app/assets/xml/hydrants.xml
+  #container = LibXML::Reader.file(filename, :options => LibXML::Parser::Options::NOBLANKS)
+  # container = XML::Reader.file(filename, :options => XML::Parser::Options::NOBLANKS |
+  #                                                   XML::Parser::Options::NOENT)
+  return XML::Reader.file(filename, :options => XML::Parser::Options::NOBLANKS | XML::Parser::Options::NOENT)
 end
 
-#TODO: add error-checking
-#TODO: refactor
-#TODO: determine best place to store
+#display a node's name: local and prefix
+def display( node )
+    return node.name, node.value
+    #puts "\tName: #{node.name}"
+    #puts " Value: #{node.value}" if node.has_value?
+end
 
-#xml to object library can work with to string to seeds.rb
+container = extract "../app/assets/xml/hydrants.xml"
+prev_name = ""
+seeds = ""
+while container.read
+  unless container.node_type == XML::Reader::TYPE_END_ELEMENT
+    #puts "Node # " + i.to_s
+    name, value = display container
+    if prev_name == "coordinates"
+      seed = "Thing.create(:city_id => 1, :lng => " + value.to_s.split(",").first + ", " + ":lat => " + value.to_s.split(",").last.split(",").first + ")"#  + value.to_s.split(",").first + " " + value.to_s.split(",").last.split(",").first
+      seeds << seed + "\n"
+    end
+    prev_name = name
+  end
+end
+#puts seeds.chop
+File.open("../db/seeds.rb", 'w') {|f| f.write(seeds.chop) } #system("rake db:seed")
+
+container.close
