4ced993fc0d34c2a340f68576b53fc7c6d9b663b
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-03 11:30:10 -0400

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ AdoptAThing::Application.configure do
   config.action_mailer.default_url_options = {:host => 'adoptahydrant.org'}
 
   # Enable threaded mode
-  config.threadsafe!
+  #config.threadsafe!
+  config.threadsafe! unless $rails_rake_task
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
