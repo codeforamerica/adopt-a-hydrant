858efbb65b11206e122c1df338f360e89e878cee
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-03 12:20:25 -0400

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ AdoptAThing::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false #look into this
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
