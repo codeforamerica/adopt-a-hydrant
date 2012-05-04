dbb91e0e094b8e03f182da1b73600e7a62fa7164
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-04 13:04:22 -0400

    adding debug statements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ protected
     if params[:flash]
       params[:flash].each do |key, message|
         flash.now[key.to_sym] = message
+        puts flash.now[key.to_sym] = message
       end
     end
   end
@@ -18,5 +19,6 @@ protected
       File.basename(file, ".yml")
     end
     I18n.locale = request.compatible_language_from(available_languages) || I18n.default_locale
+    puts "Locale: " + I18n.locale.to_s
   end
 end
