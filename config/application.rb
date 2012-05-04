90c0b8255f2adda8d4437f555f005e8573516008
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-04 14:31:32 -0400

    debugging

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module AdoptAThing
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     #config.i18n.available_locales = :en
     #config.i18n.default_locale = :en
 
