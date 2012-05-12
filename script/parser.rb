7f18ed4b4f232018a2311f6e442cb7859547651d
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-12 19:28:00 -0400

    refactor

--- script/parser.rb
+++ script/parser.rb
@@ -2,41 +2,46 @@
 # http://snippets.dzone.com/posts/show/5051
 # http://www.informit.com/articles/article.aspx?p=683059&seqNum=18
 # http://www.ruby-doc.org/docs/ProgrammingRuby/html/tut_modules.html
+# http://xml4r.github.com/libxml-ruby/rdoc/
+# http://blog.codahale.com/2005/11/24/a-ruby-howto-writing-a-method-that-uses-code-blocks/
 
 module Parser
   #require 'rubygems'
   require 'xml'
 
-  def extract(filename)
-    return XML::Reader.file(filename, :options => XML::Parser::Options::NOBLANKS | XML::Parser::Options::NOENT)
-  end
-
-  #display a node's name: local and prefix
-  def display( node )
-      return node.name, node.value
+  def extract(nodes)
+    return yield nodes
   end
   
-  def extractTransformLoadSeeds
-    container = extract "../app/assets/xml/hydrants.xml"
+  def transform(nodes)
     prev_name = ""
     seeds = ""
 
-    while container.read
-      unless container.node_type == XML::Reader::TYPE_END_ELEMENT
-        name, value = display container
-        seeds << "Thing.create(:lng => " + value.to_s.split(",").first + ", " + ":lat => " + value.to_s.split(",")[1] + ")\n" if (prev_name == "coordinates") 
-        prev_name = name
+    while nodes.read
+      unless nodes.node_type == XML::Reader::TYPE_END_ELEMENT
+        seeds << "Thing.create(:lng => " + nodes.value.to_s.split(",").first + ", " + ":lat => " + nodes.value.to_s.split(",")[1] + ")\n" if (prev_name == "coordinates") 
+        prev_name = nodes.name
       end
     end
-
+    
+    nodes.close
+    return seeds
+  end
+  
+  def load(seeds)
     File.open("../db/seeds.rb", 'w') {|f| f.write(seeds.chop)}
     system("rake db:seed")
-    container.close
   end
+  
+  def transformLoad(nodes)
+    load(transform(nodes))
+  end
+    
 end
 
 class Test
   include Parser
 end
 
-Test.new.extractTransformLoadSeeds
+test = Test.new
+test.transformLoad(test.extract("../app/assets/xml/hydrants.xml"){|nodes| XML::Reader.file("#{nodes}", :options => XML::Parser::Options::NOBLANKS | XML::Parser::Options::NOENT)})
