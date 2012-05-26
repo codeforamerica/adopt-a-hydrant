f75ac00cc0c696e696d233d20ee1f38e4d148412
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-26 19:02:37 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ AdoptAThing::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
