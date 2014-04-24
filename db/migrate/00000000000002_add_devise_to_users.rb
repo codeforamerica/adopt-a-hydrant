b229a83783974f6d7993e0c8a9b423e036e919e6
Erik Michaels-Ober <sferik@gmail.com>
2014-04-24 05:45:58 -0500

    Update rubocop to version 0.21.0

--- db/migrate/00000000000002_add_devise_to_users.rb
+++ db/migrate/00000000000002_add_devise_to_users.rb
@@ -6,18 +6,18 @@ class AddDeviseToUsers < ActiveRecord::Migration
       t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
+      t.integer :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
