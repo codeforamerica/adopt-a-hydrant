391c5c8f2a3720b809f877f333eeeff082e81a49
Benjamin Rosas <ballPointPenguin@gmx.us>
2014-09-01 12:45:00 -0500

    we don't actually support locales so why pretend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_flash_from_params
-  before_action :set_locale
 
 protected
 
@@ -13,12 +12,5 @@ protected
         flash.now[key.to_sym] = message
       end
     end
-  end
-
-  def set_locale
-    available_languages = Dir.glob(Rails.root + 'config/locales/??.yml').collect do |file|
-      File.basename(file, '.yml')
-    end
-    I18n.locale = http_accept_language.compatible_language_from(available_languages) || I18n.default_locale
   end
 end
