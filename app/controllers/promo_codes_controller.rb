18985a4a7e1df7761438e2ff76a57565a9de45dd
William Bushey <wbushey@gmail.com>
2014-09-27 22:07:57 -0500

    Wrote specs and implementation for Promo Codes controller update

--- app/controllers/promo_codes_controller.rb
+++ app/controllers/promo_codes_controller.rb
@@ -1,2 +1,20 @@
 class PromoCodesController < ApplicationController
+  respond_to :json
+
+  def update
+    return render json: {:errors => ['A user_id must be provided']}, :status => :unauthorized if !params[:promo_code][:user_id]
+    @pc = PromoCode.find_by_token(params[:promo_code][:token])
+    return render :nothing => true, :status => :not_found if @pc.nil?
+    return render json: {:errors => ['That promo code has already been used']}, :status => :unprocessable_entity if @pc.used?
+    if @pc.update_attributes(promo_code_params)
+      return render json: @pc
+    else
+      render json: {:errors => @pc.errors}, :status => 500
+    end
+  end
+
+  private
+    def promo_code_params
+      params.require(:promo_code).permit(:token, :user_id)
+    end
 end
