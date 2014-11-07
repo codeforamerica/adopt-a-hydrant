6a20f3d562c8e2f4375584f8d99f9622c7d50e70
Bill Bushey <wbushey@gmail.com>
2014-11-06 23:39:32 -0600

    Closes #29 Added awareness_code field to the Edit Profile view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101212649) do
+ActiveRecord::Schema.define(version: 20141107052217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20141101212649) do
     t.boolean  "previousEnvironmentalActivities"
     t.integer  "valueForestryWork"
     t.string   "heardOfAdoptATreeVia",            limit: nil,                              array: true
+    t.string   "awareness_code"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
