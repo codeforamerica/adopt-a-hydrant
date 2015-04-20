e8f7b36b5c0edd4684a18fc235edd1174611c95b
Erik Michaels-Ober <sferik@gmail.com>
2015-04-20 14:23:17 +0200

    Enforce code coverage minimums

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,9 @@ require 'simplecov'
 require 'coveralls'
 
 SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
-SimpleCov.start('rails')
+SimpleCov.start('rails') do
+  minimum_coverage(97.19)
+end
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
