d05de2f204bdc28ccae1f09ce20430b9f82e2eb4
Ryan Wold <wold@afomi.com>
2015-11-01 13:25:27 -0800

    add mailcatcher for development

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :assets do
 end
 
 group :development do
+  gem 'mailcatcher'
   gem 'spring'
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # For Mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
