e5a0a7140510755fe7a5e136c81424130ea19c46
Jesse Szwedko <jesse.szwedko@gmail.com>
2015-12-12 21:10:27 -0800

    Integrate simple profanity filter for names
    Using the obscenity gem
    
    Addresses #73

---
+++ test/models/thing_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ThingTest < ActiveSupport::TestCase
+  test 'name profanity filter' do
+    t = things(:thing_1)
+    t.name = 'profane aids'
+    assert_raises ActiveRecord::RecordInvalid do
+      t.save!
+    end
+  end
+end
