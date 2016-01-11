471479f5b4c335c551e66a99823753bc08fa28e0
Jesse Szwedko <jesse.szwedko@gmail.com>
2016-01-10 23:23:24 -0800

    Require obscenity in application rather than model
    Was running into issues with the load path including it directly in the
    model.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'obscenity/active_model'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
