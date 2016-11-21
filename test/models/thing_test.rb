2058021332c3d83e31847a327ccacfe1498563bb
Sid Shanker <sshanker220@gmail.com>
2016-11-21 02:58:03 -0500

    Test new drain creation.

--- test/models/thing_test.rb
+++ test/models/thing_test.rb
@@ -32,6 +32,7 @@ class ThingTest < ActiveSupport::TestCase
     fake_response = [
       'PUC_Maximo_Asset_ID,Drain_Type,System_Use_Code,Location',
       'N-11,Catch Basin Drain,ABC,"(37.75, -122.40)"',
+      'N-12,Catch Basin Drain,DEF,"(39.75, -121.40)"',
     ].join("\n")
     stub_request(:get, fake_url).to_return(body: fake_response)
 
@@ -40,6 +41,12 @@ class ThingTest < ActiveSupport::TestCase
 
     # Asserts thing_1 is deleted
     assert_nil Thing.find_by(id: thing_1.id)
+
+    # Asserts creates new drain
+    new_drain = Thing.find_by(city_id: 12)
+    assert_not_nil new_drain
+    assert_equal new_drain.lat, BigDecimal.new(39.75, 16)
+    assert_equal new_drain.lng, BigDecimal.new(-121.40, 16)
 
     # Asserts properties on thing_11 have been updated
     assert_equal thing_11.lat, BigDecimal.new(37.75, 16)
