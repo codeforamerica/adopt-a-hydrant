c0ec6a61580f31e2f19b6e9f32db729c1cfd51d6
root <root@Adoptadrain.sfgov.org>
2017-04-25 19:29:23 -0700

    DOuble quotes to single, to satisfy Travis CI build

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -5,15 +5,15 @@ class LoginTest < ActionDispatch::IntegrationTest
     @user = users(:erik)
   end
 
-  test "login with remembering" do
-    post "/users/sign_in.json", user: {email: @user.email, password: 'correct', remember_me: 1}, format: :json
+  test 'login with remembering' do
+    post '/users/sign_in.json', user: {email: @user.email, password: 'correct', remember_me: 1}, format: :json
     assert_not_empty cookies['remember_user_token']
   end
 
-  test "login without remembering" do
-    post "/users/sign_in.json", user: {email: @user.email, password: 'correct', remember_me: 1}, format: :json
-    delete "/users/sign_out.json", format: :json
-    post "/users/sign_in.json", user: {email: @user.email, password: 'correct', remember_me: 0}, format: :json
+  test 'login without remembering' do
+    post '/users/sign_in.json', user: {email: @user.email, password: 'correct', remember_me: 1}, format: :json
+    delete '/users/sign_out.json', format: :json
+    post '/users/sign_in.json', user: {email: @user.email, password: 'correct', remember_me: 0}, format: :json
     assert_empty cookies['remember_user_token']
   end
 end
