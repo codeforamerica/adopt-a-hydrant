798b72b12e2b3b842e7b96142f95720a3d2f9d8f
Jesse Szwedko <jesse.szwedko@gmail.com>
2017-07-14 14:35:29 -0700

    Adjust user seed data
    Actually save the example user and add an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,15 @@ User.where(email: 'john@example.com').first_or_initialize.tap do |user|
   user.first_name = 'John'
   user.last_name = 'Doe'
   user.password = 'password'
+  user.save!
+end
+
+User.where(email: 'admin@example.com').first_or_initialize.tap do |user|
+  user.first_name = 'Jane'
+  user.last_name = 'Doe'
+  user.password = 'password'
+  user.admin = true
+  user.save!
 end
 
 r = Random.new
